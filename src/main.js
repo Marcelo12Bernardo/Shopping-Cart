a903b4ead4b6cd83d244bea280c0cf5270c99247
Marcelo Bernardo <bernardomarcelo33@gmail.com>
2022-11-22 10:14:55 -0300

    Requisito 3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const computerList = await fetchProductsList('computador');
+computerList.forEach((element) => {
+  createProductElement(element);
+});
