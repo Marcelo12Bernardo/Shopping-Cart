4472b10194c2fd25fb2f85a0e2178656a2c25d83
Marcelo Bernardo <bernardomarcelo33@gmail.com>
2022-11-23 10:32:13 -0300

    Requisito 4

--- src/main.js
+++ src/main.js
@@ -6,23 +6,28 @@ import { createProductElement } from './helpers/shopFunctions';
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 const secaoProdutos = document.querySelector('.products');
 
-// const removeLoad = () => {
-//   const loading = document.querySelector('.loading');
-//   loading.remove();
-// };
+const removeLoad = () => {
+  const loading = document.querySelector('.loading');
+  loading.remove();
+};
 
-// const addLoad = () => {
-//   const loading = document.createElement('span');
-//   loading.className = 'loading';
-//   loading.textContent = 'carregando...';
-//   secaoProdutos.appendChild(loading);
-// };
+const addLoad = () => {
+  const loading = document.createElement('span');
+  loading.className = 'loading';
+  loading.textContent = 'carregando...';
+  secaoProdutos.appendChild(loading);
+};
 window.onload = async () => {
-  const computerList = await fetchProductsList('computador');
-  computerList.forEach((element) => {
-    const novaSecao = createProductElement(element);
-    secaoProdutos.appendChild(novaSecao);
-  });
-  // addLoad();
-  // removeLoad();
+  addLoad();
+  try {
+    const computerList = await fetchProductsList('computador');
+    removeLoad();
+    computerList.forEach((element) => {
+      const novaSecao = createProductElement(element);
+      secaoProdutos.appendChild(novaSecao);
+    });
+  } catch (erro) {
+    removeLoad();
+    // Error();
+  }
 };
