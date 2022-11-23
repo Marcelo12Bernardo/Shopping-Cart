8e7279ebdfa69bc4c58fc57cc4f455ec134c4859
Marcelo Bernardo <bernardomarcelo33@gmail.com>
2022-11-23 10:09:23 -0300

    lint

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
-import { fetchProductsList } from './helpers/fetchFunctions';
-import { createProductElement } from './helpers/shopFunctions';
+// import { fetchProductsList } from './helpers/fetchFunctions';
+// import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 const secaoProdutos = document.querySelector('.products');
@@ -24,4 +24,5 @@ window.onload = async () => {
   //   secaoProdutos.appendChild(novaSecao);
   // });
   addLoad();
+  removeLoad();
 };
